343ae53deca5703e654f8d78059a3c5ab0ed3fb6
DEEPAK MISHRA <deepak.win8@gmail.com>
2025-12-20 03:20:12 +0530

    Experimental CSS

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
   /* config options here */
   images: {
     domains: ['ap-south-1.graphassets.com'],
-    minimumCacheTTL: 31536000,
+    minimumCacheTTL: 31536000
+  },
+  experimental: {
+    inlineCss: true,
+    cssChunking: true
   }
 };
 
