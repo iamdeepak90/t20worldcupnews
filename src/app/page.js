352d3668c1a7bb11737500ba80a836d4ddab23fa
DEEPAK MISHRA <73831726+iamdeepak90@users.noreply.github.com>
2025-12-26 06:01:13 +0530

    blur img

--- src/app/page.js
+++ src/app/page.js
@@ -81,6 +81,8 @@ export default async function Home() {
               height={181}
               alt={fpost.coverImage.altText}
               className="thumb"
+              placeholder="blur"
+              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAFCAYAAAB4ka1VAAAAk0lEQVR4ARyMsQmAMBREzzQWgoM4hhs4hSu4gAtYuJOFhWItKEqakEBIQggkX0x7995jbdtS3/c0jiPN80zTNNEwDNR1HTVNQ8wYA2stiqJAVVWo6xplWSKlhBgjmFIKnHM8z4PrunDfN973hRACzjkwrXUe933Huq5YlgXbtmXorzPvPaSUOM8zH8dxZOEvhxDwAQAA//+Ro3vUAAAABklEQVQDAFlyXgftTnIBAAAAAElFTkSuQmCC"
             />
 
             <div className="row-between card-topline">
--- src/components/RecentPosts.js
+++ src/components/RecentPosts.js
@@ -12,10 +12,9 @@ export default async function RecentPosts(){
       <Link key={post.slug} className="recent-item" href={`/${post.slug}`}>
         <Image
             src={post.coverImage.url}
-            width={100}
-            height={100}
+            width={78}
+            height={90}
             alt={post.coverImage.altText}
-            priority
             placeholder="blur"
             blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAFCAYAAAB4ka1VAAAAk0lEQVR4ARyMsQmAMBREzzQWgoM4hhs4hSu4gAtYuJOFhWItKEqakEBIQggkX0x7995jbdtS3/c0jiPN80zTNNEwDNR1HTVNQ8wYA2stiqJAVVWo6xplWSKlhBgjmFIKnHM8z4PrunDfN973hRACzjkwrXUe933Huq5YlgXbtmXorzPvPaSUOM8zH8dxZOEvhxDwAQAA//+Ro3vUAAAABklEQVQDAFlyXgftTnIBAAAAAElFTkSuQmCC"
           />
