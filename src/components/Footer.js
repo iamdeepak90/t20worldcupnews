4c9cdb52a8b3e4c5d2f6abfb748b0dfed0d75b31
DEEPAK MISHRA <73831726+iamdeepak90@users.noreply.github.com>
2026-01-01 20:57:35 +0530

    Robots.txt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(){
   return (
 <footer className="site-footer">
   <div className="container footer-row">
-    <div>© T20 World Cup 2026 | This is not an official website.</div>
+    <div>© T20 World Cup 2026 | Not an official site</div>
     <div className="footer-links">
       <Link href="/about-us">About Us</Link>
       <Link href="/disclaimer">Disclaimer</Link>
